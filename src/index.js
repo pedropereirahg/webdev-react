2a0d2149da9726f00117e9b1a9ed841035adf14f
Rodolfo Costa <rodolfocostaclemente@gmail.com>
2017-12-01 16:05:51 -0200

    Added material-ui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MaterialApp = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(<MaterialApp />, document.getElementById("root"));
+
 registerServiceWorker();
